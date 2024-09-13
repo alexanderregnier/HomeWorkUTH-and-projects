5f52cf695b425a9daa0ac0cedb9a81f21b043ce9
Sadrach Juan Diego Garcia Flores <Jdiego0805@gmail.com>
2024-09-12 23:10:24 -0700

    Add files via upload

---
+++ ensambladora/Ensambladora/bin/Debug/Ensambladora.bat
@@ -0,0 +1,4 @@
+@REM instalar framework de visual studio
+@REM instalar WampServer
+C:\wamp64\bin\mariadb\mariadb10.2.14\bin\mysql -uroot -p1234 < ensambladora.sql
+copy ensambladora.lnk C:\Users\Sadrach\Desktop
